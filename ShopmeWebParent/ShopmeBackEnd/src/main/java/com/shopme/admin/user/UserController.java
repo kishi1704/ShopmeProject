c6c3e15f4a96f6c5f52e1d624a52373c85a42e35
ttruong52 <tienvan.truong@dxc.com>
2023-09-04 11:00:27 +0700

    create Users Listing Function

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/user/UserController.java
@@ -1,8 +1,13 @@
 package com.shopme.admin.user;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.shopme.common.entity.User;
 
 @Controller
 public class UserController {
@@ -11,7 +16,10 @@ public class UserController {
 	private UserService service;
 	
 	@GetMapping("/users")
-	public String listAll() {
+	public String listAll(Model model) {
+		List<User> listUsers = service.listAll();
+		model.addAttribute("listUsers", listUsers);
+		
 		return "users";
 	}
 }
