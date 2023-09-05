b049e0f7fdf5f7f539c3cc75ed926ccd734d17f4
ttruong52 <tienvan.truong@dxc.com>
2023-09-05 17:17:58 +0700

    encode User Password

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/RoleRepositoryTests.java
@@ -3,7 +3,6 @@ package com.shopme.admin.user;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import java.util.ArrayList;
-import java.util.List;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
