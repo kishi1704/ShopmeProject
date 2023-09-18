26d206719fe5756a9e3a1045d63577b033cdfe2e
ttruong52 <tienvan.truong@dxc.com>
2023-09-18 16:25:52 +0700

    show User's fullname and Logout link

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/ShopmeUserDetails.java
@@ -65,4 +65,7 @@ public class ShopmeUserDetails implements UserDetails {
 		return user.isEnabled();
 	}
 
+	public String getFullname() {
+		return this.user.getFirstName() + " " + this.user.getLastName();
+	}
 }
