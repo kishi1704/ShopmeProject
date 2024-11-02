ff22479fb5f83a878f9f4efb3a6d405dc1624090
Kishi <98truongtien@gmail.com>
2024-11-02 18:39:40 +0700

    Code User Management - code filter function for users list

--- ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
+++ ShopmeWebParent/ShopmeBackEnd/src/test/java/com/shopme/admin/user/UserRepositoryTests.java
@@ -128,4 +128,18 @@ public class UserRepositoryTests {
 		assertThat(listUsers.size()).isEqualTo(pageSize);
 	}
 
+	@Test
+	public void testSearchUser() {
+		String keyword = "bruce";
+		int pageNumber = 0;
+		int pageSize = 4;
+		
+		Pageable pageable = PageRequest.of(pageNumber, pageSize);
+		Page<User> page = repo.findAll(keyword, pageable);
+		
+		List<User> listUsers = page.getContent();
+		listUsers.forEach(user -> System.out.println(user));
+		
+		assertThat(listUsers.size()).isGreaterThan(0);
+	}
 }
