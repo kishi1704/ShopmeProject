59141a9703ee519eef0f628633c6f0296fa09776
Kishi <98truongtien@gmail.com>
2024-11-11 21:26:37 +0700

    Code User Authentication - show user full name and logout link

--- ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
+++ ShopmeWebParent/ShopmeBackEnd/src/main/java/com/shopme/admin/security/WebSecurityConfig.java
@@ -2,21 +2,37 @@ package com.shopme.admin.security;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.security.authentication.dao.DaoAuthenticationProvider;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
 public class WebSecurityConfig {
+	
+	@Bean
+	UserDetailsService userDetailsService() {
+		return new ShopmeUserDetailsService();
+	}
 
     @Bean
     PasswordEncoder passwordEncoder() {
 		return new BCryptPasswordEncoder();
 	}
+    
+    DaoAuthenticationProvider authenticationProvider() {
+    	DaoAuthenticationProvider authProvider = new DaoAuthenticationProvider();
+    	authProvider.setUserDetailsService(userDetailsService());
+    	authProvider.setPasswordEncoder(passwordEncoder());
+    	
+    	return authProvider;
+    }
 	
 	@Bean
 	SecurityFilterChain configureHttpSecurity(HttpSecurity http) throws Exception {
+		http.authenticationProvider(authenticationProvider());
 		
 		http.authorizeHttpRequests(auth -> auth
 				.requestMatchers("/images/**", "/js/**", "/webjars/**").permitAll()
